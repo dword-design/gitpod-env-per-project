d088f8c23e85e8224713b7f8993b963ebd6a5af4
Sebastian Landwehr <info@dword-design.de>
2021-04-09 00:07:48 +0200

    fix: fix special characters (#1)

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,8 @@
-import { endent } from '@dword-design/functions'
+import { endent, property } from '@dword-design/functions'
 import tester from '@dword-design/tester'
 import testerPluginEnv from '@dword-design/tester-plugin-env'
 import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
+import execa from 'execa'
 import { outputFile } from 'fs-extra'
 
 import self from '.'
@@ -10,22 +11,119 @@ export default tester(
   [
     async () => {
       await outputFile('package.json', JSON.stringify({ name: 'pack' }))
-      process.env.PACK_FOO = 'test'
-      process.env.PACK_BAR = 'test2'
-      expect(await self()).toEqual(endent`
-        export PACK_BAR=
-        export PACK_FOO=
-        export BAR="test2"
-        export FOO="test"
+      expect(
+        (await execa.command(
+          `eval $("${require.resolve(
+            './cli'
+          )}") && echo "$FOO" && echo "$BAR" && echo "$PACK_FOO" && echo "$PACK_BAR"`,
+          {
+            env: {
+              PACK_BAR: 'test2',
+              PACK_FOO: 'test',
+            },
+            shell: true,
+          }
+        ))
+          |> await
+          |> property('stdout')
+      ).toEqual(endent`
+        test
+        test2
+
+
       `)
     },
     async () => {
       await outputFile('package.json', JSON.stringify({ name: '@scope/pack' }))
-      process.env.PACK_FOO = 'test'
-      expect(await self()).toEqual(endent`
-          export PACK_FOO=
-          export FOO="test"
-        `)
+      expect(
+        (await execa.command(
+          `eval $("${require.resolve(
+            './cli'
+          )}") && echo "$FOO" && echo "$PACK_FOO"`,
+          {
+            env: {
+              PACK_FOO: 'test',
+            },
+            shell: true,
+          }
+        ))
+          |> await
+          |> property('stdout')
+      ).toEqual(endent`
+        test
+
+      `)
+    },
+    async () => {
+      await outputFile('package.json', JSON.stringify({ name: 'pack' }))
+      expect(
+        (await execa.command(
+          `eval $("${require.resolve('./cli')}") && echo "$FOO"`,
+          {
+            env: {
+              PACK_FOO: 'test  test2',
+            },
+            shell: true,
+          }
+        ))
+          |> await
+          |> property('stdout')
+      ).toEqual('test  test2')
+    },
+    async () => {
+      await outputFile('package.json', JSON.stringify({ name: 'pack' }))
+      expect(
+        (await execa.command(
+          `eval $("${require.resolve('./cli')}") && echo "$FOO"`,
+          {
+            env: {
+              PACK_FOO: '{ "test": "test", "test2": "test2" }',
+            },
+            shell: true,
+          }
+        ))
+          |> await
+          |> property('stdout')
+      ).toEqual('{ "test": "test", "test2": "test2" }')
+    },
+    async () => {
+      await outputFile('package.json', JSON.stringify({ name: 'pack' }))
+      expect(
+        (await execa.command(
+          `eval $("${require.resolve('./cli')}") && echo "$FOO"`,
+          {
+            env: {
+              PACK_FOO: endent`
+                {
+                  "test": "test",
+                  "test2": "test2"
+                }
+              `,
+            },
+            shell: true,
+          }
+        ))
+          |> await
+          |> property('stdout')
+      ).toEqual(endent`
+        {
+          "test": "test",
+          "test2": "test2"
+        }
+      `)
+    },
+    async () => {
+      await outputFile('package.json', JSON.stringify({ name: 'pack' }))
+      expect(
+        execa.command(`eval $("${require.resolve('./cli')}") && echo "$FOO"`, {
+          env: {
+            PACK_FOO: 'bar" --bla"',
+          },
+          shell: true,
+        })
+          |> await
+          |> property('stdout')
+      ).toEqual('bar" --bla"')
     },
     () =>
       expect(self()).rejects.toThrow(
